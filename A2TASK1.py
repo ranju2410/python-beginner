440db5a1139abf34a691f669bc65af122db0a9bd
ranju2410 <granjith.sree@gmail.com>
2025-06-21 20:38:12 +0530

    Add files via upload

---
+++ A2TASK1.py
@@ -0,0 +1,7 @@
+n=int(input("Enter a Number : "))
+
+
+if n%2==0:
+    print(n," is an even number")
+else:
+    print(n," is an odd number")
---
+++ A2TASK2.py
@@ -0,0 +1,6 @@
+
+k=0
+for i in range(50):
+    k=k+(i+1)
+
+print(k)
